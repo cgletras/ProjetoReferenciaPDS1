05dcabc6de82252c73d563aa8d912876cf408ccc
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-08-20 22:27:16 -0300

    Projeto criado

---
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/ProjetoReferenciaPds1Application.java
@@ -0,0 +1,13 @@
+package com.ProjetoReferenciaPDS1.ProjetoRefPDS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReferenciaPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReferenciaPds1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/ProjetoReferenciaPds1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ProjetoReferenciaPDS1.ProjetoRefPDS1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoReferenciaPds1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
