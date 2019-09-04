eec733c8f06302f220d552269643e4e3338a14d1
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-09-03 21:13:39 -0300

    Order, instant, ISO 8601 - part 2

---
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.Order;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
