1e707884be145d8624464b31c9586ff10b0c17ac
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-09-17 02:13:25 -0300

    Category Exception handling - update

--- src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/CategoryService.java
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/CategoryService.java
@@ -3,6 +3,8 @@ package com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,13 @@ public class CategoryService {
 	}
 	
 	public Category update(Long id, Category obj) {
-		Category entity = repository.getOne(id);
-		updateData(entity, obj);
-		return  repository.save(entity);
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj);
+			return  repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(Category entity, Category obj) {
