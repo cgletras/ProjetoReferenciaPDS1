40badeb8c3eb27a936091f81e18683f361d4f4ae
Carlos <cgletras@gmail.com>
2019-10-16 16:21:48 -0300

    Product Update

--- src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/resources/ProductResource.java
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/resources/ProductResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.ProductCategoriesDTO;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.ProductDTO;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.UserDTO;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services.ProductService;
 
 @RestController
@@ -42,5 +44,11 @@ public class ProductResource {
 		ProductDTO newDto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto){
+		ProductDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 }
