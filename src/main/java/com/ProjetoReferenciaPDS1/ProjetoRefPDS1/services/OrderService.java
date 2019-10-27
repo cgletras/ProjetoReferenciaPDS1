0f788a1a4e06caea7019e920a140842924571116
Carlos <cgletras@gmail.com>
2019-10-26 22:06:46 -0300

    Order GET endpoints PART 2

--- src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/OrderService.java
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/OrderService.java
@@ -15,6 +15,7 @@ import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.Order;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.OrderItem;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.User;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.OrderRepository;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.UserRepository;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -25,6 +26,9 @@ public class OrderService {
 	
 	@Autowired
 	private AuthService authService; 
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAll();
@@ -50,5 +54,12 @@ public class OrderService {
 		authService.validadeOwnOrderOrAdmin(order);
 		Set<OrderItem> set = order.getItems();
 		return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public List<OrderDTO> findByClientId(Long clientId) {
+		User client = userRepository.getOne(clientId);
+		List<Order> list = repository.findByClient(client);
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 }
