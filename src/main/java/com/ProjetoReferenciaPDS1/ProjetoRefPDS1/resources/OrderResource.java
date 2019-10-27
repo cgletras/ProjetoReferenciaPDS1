0e8602f2de07a6b946c7e7ee81aed77502f81143
Carlos <cgletras@gmail.com>
2019-10-27 00:40:53 -0300

    Place order endpoint

--- src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/resources/OrderResource.java
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/resources/OrderResource.java
@@ -1,5 +1,6 @@
 package com.ProjetoReferenciaPDS1.ProjetoRefPDS1.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.OrderDTO;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.OrderItemDTO;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.Order;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services.OrderService;
 
 @RestController
@@ -56,4 +61,11 @@ public class OrderResource {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@PostMapping
+	public ResponseEntity<OrderDTO> placeOrder(@RequestBody List<OrderItemDTO> dto){
+		OrderDTO orderDTO = service.placeOrder(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(orderDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(orderDTO);
+	}
 }
--- src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/OrderService.java
+++ src/main/java/com/ProjetoReferenciaPDS1/ProjetoRefPDS1/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -13,8 +14,12 @@ import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.OrderDTO;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.dto.OrderItemDTO;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.Order;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.OrderItem;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.Product;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.User;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.entities.enums.OrderStatus;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.OrderItemRepository;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.OrderRepository;
+import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.ProductRepository;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.repositories.UserRepository;
 import com.ProjetoReferenciaPDS1.ProjetoRefPDS1.services.exceptions.ResourceNotFoundException;
 
@@ -29,6 +34,12 @@ public class OrderService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAll();
@@ -61,5 +72,24 @@ public class OrderService {
 		User client = userRepository.getOne(clientId);
 		List<Order> list = repository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO placeOrder(List<OrderItemDTO> dto) {
+
+		User client = authService.authenticated();
+		Order order = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, client);
+		
+		for(OrderItemDTO itemDTO: dto) {
+			Product product = productRepository.getOne(itemDTO.getProductId());
+			OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), itemDTO.getPrice());
+			order.getItems().add(item);
+		}
+		
+		repository.save(order);
+		orderItemRepository.saveAll(order.getItems());
+		
+		return new OrderDTO(order);
+		
 	}
 }
